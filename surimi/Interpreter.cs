a447d5b737970cd24d173e8cd3b5c8a40b437a24
Derrick W. Turk <dwt@terminusdatascience.com>
2023-01-29 21:12:37 -0600

    evaluate, but I need to tag things with line numbers

--- surimi/Interpreter.cs
+++ surimi/Interpreter.cs
@@ -12,14 +12,47 @@ public class Interpreter {
     public void run(string code)
     {
         var expr = Parser.Parse(Lexer.Lex(code, _onError), _onError);
-        if (!_onError.HadError) {
-            Console.WriteLine(
-              $"Expression: {expr!.Accept(new PrettyPrinter())}");
+        if (_onError.HadError)
             return;
+
+        object? v = EvaluateExpression(expr!);
+        if (_onError.HadError)
+            return;
+
+        Console.WriteLine(ValueString(v));
+    }
+
+    private object? EvaluateExpression(Expr expr)
+    {
+        try {
+            return expr.Accept(new ExprEvaluator());
+        } catch (TypeError e) {
+            _onError.Error(
+              /* what, you coward? you don't have line numbers here. */0,
+              e.Payload);
+            return null;
+        } catch (DivZeroError) {
+            _onError.Error(
+              /* what, you coward? you don't have line numbers here. */0,
+              "division by zero");
+            return null;
         }
     }
 
     private ErrorReporter _onError;
+
+    private static bool ValueEquals(object? lhs, object? rhs) =>
+        lhs == null ? rhs == null : lhs.Equals(rhs);
+
+    private static string ValueString(object? val) => val switch
+    {
+        null => "nil",
+        true => "true",
+        false => "false",
+        double d => d.ToString(),
+        string s => $"\"{s}\"",
+        _ => throw new InvalidOperationException("invalid runtime value"),
+    };
 
     private class TypeError: Exception {
         public TypeError(string message)
@@ -35,78 +68,69 @@ public class Interpreter {
     private class ExprEvaluator: Visitor<object?> {
         public object? VisitLiteral(Literal e) => e.Value;
 
-        public object VisitUnOpApp(UnOpApp e)
-        {
-            var operand = e.Operand.Accept(this);
-            switch (e.Operator)
+        public object VisitUnOpApp(UnOpApp e) =>
+            (e.Operator, e.Operand.Accept(this)) switch
             {
-                case UnOp.Not:
-                    if (operand is bool b)
-                        return !b;
-                    throw new TypeError("operand to ! must be boolean");
-
-                case UnOp.Negate:
-                    if (operand is double d)
-                        return -d;
-                    throw new TypeError("operand to - must be number");
-            }
-        }
+                (UnOp.Not, bool b) => !b,
+                (UnOp.Not, _) =>
+                    throw new TypeError("operand to ! must be boolean"),
+                (UnOp.Negate, double d) => -d,
+                (UnOp.Negate, _) =>
+                    throw new TypeError("operand to - must be number"),
+                _ => throw new InvalidOperationException(
+                  "invalid unary operator"),
+            };
 
         public object VisitBinOpApp(BinOpApp e)
         {
             // TODO: lazy cases check here
 
-            var lhs = e.Lhs.Accept(this);
-            var rhs = e.Rhs.Accept(this);
-
-            switch (e.Operator)
+            return (e.Operator, e.Lhs.Accept(this), e.Rhs.Accept(this)) switch
             {
-                case BinOp.Plus:
-                    if (lhs is double dlhs && rhs is double drhs)
-                        return dlhs + drhs;
-                    if (lhs is string slhs && rhs is string srhs)
-                        return slhs + srhs;
-                    throw new TypeError(
-                      "operands to + must be both numbers or both strings");
+                (BinOp.Plus, double lhs, double rhs) => lhs + rhs,
+                (BinOp.Plus, string lhs, string rhs) => lhs + rhs,
+                (BinOp.Plus, double _, string _) =>
+                    throw new TypeError("operands to + must have matching types"),
+                (BinOp.Plus, string _, double _) =>
+                    throw new TypeError("operands to + must have matching types"),
+                (BinOp.Plus, _, _) =>
+                    throw new TypeError("operands to + must be numbers or strings"),
 
-                case BinOp.Minus:
-                    if (lhs is double dlhs && rhs is double drhs)
-                        return dlhs - drhs;
-                    throw new TypeError("operands to - must be numbers");
+                (BinOp.Minus, double lhs, double rhs) => lhs - rhs,
+                (BinOp.Minus, _, _) =>
+                    throw new TypeError("operands to - must be numbers"),
 
-                case BinOp.Times:
-                    if (lhs is double dlhs && rhs is double drhs)
-                        return dlhs * drhs;
-                    throw new TypeError("operands to * must be numbers");
+                (BinOp.Times, double lhs, double rhs) => lhs * rhs,
+                (BinOp.Times, _, _) =>
+                    throw new TypeError("operands to * must be numbers"),
 
-                case BinOp.DividedBy:
-                    if (lhs is double dlhs && rhs is double drhs) {
-                        if (drhs == 0.0)
-                            throw new DivZeroError();
-                        return lhs / rhs;
-                    }
-                    throw new TypeError("operands to / must be numbers");
+                (BinOp.DividedBy, double _, 0.0) => throw new DivZeroError(),
+                (BinOp.DividedBy, double lhs, double rhs) => lhs / rhs,
+                (BinOp.DividedBy, _, _) =>
+                    throw new TypeError("operands to / must be numbers"),
 
-                case BinOp.Lt:
-                    if (lhs is double dlhs && rhs is double drhs)
-                        return dlhs < drhs;
-                    throw new TypeError("operands to < must be numbers");
+                (BinOp.EqEq, var lhs, var rhs) => ValueEquals(lhs, rhs),
+                (BinOp.NotEq, var lhs, var rhs) => !ValueEquals(lhs, rhs),
 
-                case BinOp.LtEq:
-                    if (lhs is double dlhs && rhs is double drhs)
-                        return dlhs <= drhs;
-                    throw new TypeError("operands to <= must be numbers");
+                (BinOp.Lt, double lhs, double rhs) => lhs < rhs,
+                (BinOp.Lt, _, _) =>
+                    throw new TypeError("operands to < must be numbers"),
 
-                case BinOp.Gt:
-                    if (lhs is double dlhs && rhs is double drhs)
-                        return dlhs > drhs;
-                    throw new TypeError("operands to > must be numbers");
+                (BinOp.LtEq, double lhs, double rhs) => lhs <= rhs,
+                (BinOp.LtEq, _, _) =>
+                    throw new TypeError("operands to <= must be numbers"),
 
-                case BinOp.GtEq:
-                    if (lhs is double dlhs && rhs is double drhs)
-                        return dlhs >= drhs;
-                    throw new TypeError("operands to >= must be numbers");
-            }
+                (BinOp.Gt, double lhs, double rhs) => lhs > rhs,
+                (BinOp.Gt, _, _) =>
+                    throw new TypeError("operands to > must be numbers"),
+
+                (BinOp.GtEq, double lhs, double rhs) => lhs >= rhs,
+                (BinOp.GtEq, _, _) =>
+                    throw new TypeError("operands to >= must be numbers"),
+
+                _ => throw new InvalidOperationException(
+                  "invalid binary operator"),
+            };
         }
     }
 }
