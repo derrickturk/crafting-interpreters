e502a91ba5911502bbe329d223f40096023ea5ba
Derrick W. Turk <dwt@terminusdatascience.com>
2023-03-14 15:28:54 -0500

    begin ch11; boilerplate traversal

--- surimi/Visitor.cs
+++ surimi/Visitor.cs
@@ -19,3 +19,93 @@ public interface StmtVisitor<T> {
     public T VisitVarDecl(VarDecl s);
     public T VisitFunDef(FunDef s);
 }
+
+// a "template" traversal which just visits things and does nothing
+public class Traverser: ExprVisitor<ValueTuple>, StmtVisitor<ValueTuple> {
+    public ValueTuple VisitLiteral(Literal e) => ValueTuple.Create();
+
+    public ValueTuple VisitUnOpApp(UnOpApp e) => e.Operand.Accept(this);
+
+    public ValueTuple VisitBinOpApp(BinOpApp e)
+    {
+        e.Lhs.Accept(this);
+        e.Rhs.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitVar(Var e) => ValueTuple.Create();
+
+    public ValueTuple VisitAssign(Assign e)
+    {
+        e.Variable.Accept(this);
+        e.Value.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitCall(Call e)
+    {
+        e.Callee.Accept(this);
+        foreach (var expr in e.Arguments)
+            expr.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitExprStmt(ExprStmt s)
+    {
+        s.Expression.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitIfElse(IfElse s)
+    {
+        s.Condition.Accept(this);
+        s.If.Accept(this);
+        if (s.Else != null)
+            s.Else.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitWhile(While s)
+    {
+        s.Condition.Accept(this);
+        s.Body.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitPrint(Print s)
+    {
+        s.Expression.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitReturn(Return s)
+    {
+        s.Expression.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitBlock(Block s)
+    {
+        foreach (var stmt in s.Statements)
+            stmt.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitVarDecl(VarDecl s)
+    {
+        s.Variable.Accept(this);
+        if (s.Initializer != null)
+            s.Initializer.Accept(this);
+        return ValueTuple.Create();
+    }
+
+    public ValueTuple VisitFunDef(FunDef s)
+    {
+        s.Name.Accept(this);
+        foreach (var p in s.Parameters)
+            p.Accept(this);
+        foreach (var stmt in s.Body)
+            stmt.Accept(this);
+        return ValueTuple.Create();
+    }
+}
