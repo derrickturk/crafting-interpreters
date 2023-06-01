f0e271659313a12f30fd042aab690005a61cfe4f
Derrick W. Turk <dwt@terminusdatascience.com>
2023-06-01 15:48:40 -0500

    debug mode w/ tracing

--- dishwasher_salmon/src/vm.c
+++ dishwasher_salmon/src/vm.c
@@ -3,6 +3,10 @@
 #include "ds/value.h"
 #include "ds/vm.h"
 
+#ifdef DS_DEBUG_TRACE
+#include "ds/debug.h"
+#endif
+
 inline static uint8_t fetch_code(ds_vm *vm)
 {
     return *vm->ip++;
@@ -25,18 +29,23 @@ inline static ds_value fetch_const_long(ds_vm *vm)
 ds_vm_result ds_vm_run(ds_vm *vm)
 {
     for (;;) {
+#ifdef DS_DEBUG_TRACE
+        ds_disassemble_instr(vm->chunk, vm->ip - vm->chunk->code.data);
+#endif
         switch (fetch_code(vm)) {
             case DS_OP_RETURN:
                 return DS_VM_OK;
             case DS_OP_CONST:
                 {
                     ds_value v = fetch_const(vm);
+                    // TODO: temporary, debug only
                     ds_value_print(v);
                     break;
                 }
             case DS_OP_CONST_LONG:
                 {
                     ds_value v = fetch_const_long(vm);
+                    // TODO: temporary, debug only
                     ds_value_print(v);
                     break;
                 }
