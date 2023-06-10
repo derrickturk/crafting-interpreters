03056f6799f566b5cded162813d61a5a3da65b11
Derrick W. Turk <dwt@terminusdatascience.com>
2023-06-09 20:49:16 -0500

    stack

--- dishwasher_salmon/include/ds/vm.h
+++ dishwasher_salmon/include/ds/vm.h
@@ -3,6 +3,7 @@
 
 #include "ds/common.h"
 #include "ds/bytecode.h"
+#include "ds/value.h"
 
 typedef enum ds_vm_result {
     DS_VM_OK,
@@ -10,15 +11,22 @@ typedef enum ds_vm_result {
     DS_VM_RUNTIME_ERROR,
 } ds_vm_result;
 
+enum {
+    DS_VM_STACK_MAX = 256,
+};
+
 typedef struct ds_vm {
     const ds_chunk *chunk;
     const uint8_t *ip;
+    ds_value stack[DS_VM_STACK_MAX];
+    ds_value* stack_top;
 } ds_vm;
 
 static inline void ds_vm_init(ds_vm *vm)
 {
     vm->chunk = NULL;
     vm->ip = NULL;
+    vm->stack_top = &vm->stack[0];
 }
 
 static inline void ds_vm_free(ds_vm *vm)
@@ -33,6 +41,18 @@ static inline ds_vm_result ds_vm_interpret(ds_vm *vm, const ds_chunk *chunk)
     vm->chunk = chunk;
     vm->ip = chunk->code.data;
     return ds_vm_run(vm);
+}
+
+// TODO: make this safe after we're sure it's sticking around
+static inline void ds_vm_stack_push(ds_vm *vm, ds_value value)
+{
+    *vm->stack_top++ = value;
+}
+
+// TODO: make this safe after we're sure it's sticking around
+static inline ds_value ds_vm_stack_pop(ds_vm *vm)
+{
+    return *--vm->stack_top;
 }
 
 #endif
--- dishwasher_salmon/src/vm.c
+++ dishwasher_salmon/src/vm.c
@@ -4,6 +4,8 @@
 #include "ds/vm.h"
 
 #ifdef DS_DEBUG_TRACE
+#include <stdio.h>
+
 #include "ds/debug.h"
 #endif
 
@@ -30,23 +32,30 @@ ds_vm_result ds_vm_run(ds_vm *vm)
 {
     for (;;) {
 #ifdef DS_DEBUG_TRACE
+        printf("    ");
+        for (ds_value *v = vm->stack; v < vm->stack_top; ++v) {
+            printf("[ ");
+            ds_value_print(*v);
+            printf("] ");
+        }
+        printf("\n");
         ds_disassemble_instr(vm->chunk, vm->ip - vm->chunk->code.data);
 #endif
         switch (fetch_code(vm)) {
             case DS_OP_RETURN:
+                // TODO: temporary
+                ds_value_print(ds_vm_stack_pop(vm));
                 return DS_VM_OK;
             case DS_OP_CONST:
                 {
                     ds_value v = fetch_const(vm);
-                    // TODO: temporary, debug only
-                    ds_value_print(v);
+                    ds_vm_stack_push(vm, v);
                     break;
                 }
             case DS_OP_CONST_LONG:
                 {
                     ds_value v = fetch_const_long(vm);
-                    // TODO: temporary, debug only
-                    ds_value_print(v);
+                    ds_vm_stack_push(vm, v);
                     break;
                 }
         }
